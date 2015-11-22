c26428b4f470eb2793ffe67c8ab8bec0dba654fb
Armanious <armanious@live.com>
2015-11-21 19:28:16 -0500

    Fixed all bugs in Stack Manipulator
    Does not work as expected: TreeMap#entrySet().toArray(). Used two-array
    with a binary-search-backed insertion algorithm for descending list
    size order.

--- src/org/armanious/csci1260/obfuscation/StackManipulatorBeta.java
+++ src/org/armanious/csci1260/obfuscation/StackManipulatorBeta.java
@@ -1,18 +1,17 @@
 package org.armanious.csci1260.obfuscation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Random;
-import java.util.Set;
 import java.util.TreeMap;
 
 import org.armanious.csci1260.DataManager;
-import org.armanious.csci1260.DataManager.BasicBlock;
-import org.armanious.csci1260.DataManager.ConstructorTemporary;
+import org.armanious.csci1260.DataManager.InvokeSpecialTemporary;
 import org.armanious.csci1260.DataManager.MethodInformation;
-import org.armanious.csci1260.DataManager.MethodInvocationTemporary;
 import org.armanious.csci1260.DataManager.Temporary;
 import org.armanious.csci1260.JavaStack;
 import org.armanious.csci1260.Tuple;
@@ -22,7 +21,6 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.VarInsnNode;
-import org.objectweb.asm.util.Textifier;
 
 public class StackManipulatorBeta implements Opcodes {
 
@@ -32,16 +30,74 @@ public class StackManipulatorBeta implements Opcodes {
 		this.dm = dm;
 	}
 
-	private BasicBlock getBlockInstructionIsIn(AbstractInsnNode a, MethodInformation ctx){
-		BasicBlock block;
-		do {
-			block = ctx.blocks.get(a);
-			a = a.getPrevious();
-		}while(block == null && a != null);
-		return block;
-	}
-
 	public void obfuscate(){
+		int count = 0;
+		for(MethodInformation mi : dm.methodInformations.values()){
+			final Temporary[] tmps = mi.temporaries.toArray(new Temporary[mi.temporaries.size()]);
+			@SuppressWarnings("unchecked") //sigh runtime type erasure
+			final ArrayList<AbstractInsnNode>[] blocks = (ArrayList<AbstractInsnNode>[]) new ArrayList[tmps.length];
+			final Comparator<ArrayList<AbstractInsnNode>> descendingComparator = (A,B)->(B == null ? -1 : B.size()) - (A == null ? -1 : A.size());
+			for(int i = 0; i < tmps.length; i++){
+				final Temporary tmp = tmps[i];
+				final ArrayList<AbstractInsnNode> block = tmp.getContiguousBlock();
+				if(block == null) continue;
+				
+				int index = Arrays.binarySearch(blocks, block, descendingComparator);
+				if(index < 0) index = ~index;
+				
+				System.arraycopy(blocks, index, blocks, index + 1, blocks.length - index - 1);
+				blocks[index] = block;
+				System.arraycopy(tmps, index, tmps, index + 1, tmps.length - index - 1);
+				tmps[index] = tmp;
+			}
+			
+			for(int i = 0; i < blocks.length; i++){
+				final ArrayList<AbstractInsnNode> block = blocks[i];
+				if(block == null) continue;
+				
+				final int start = block.get(0).getIndex();
+				final int end = block.get(block.size() - 1).getIndex();
+				
+				for(int j = i + 1; j < blocks.length; j++){
+					final ArrayList<AbstractInsnNode> list2 = blocks[j];
+					if(list2 == null) continue;
+					
+					final int start2 = list2.get(0).getIndex();
+					final int end2 = list2.get(list2.size() - 1).getIndex();
+					
+					if((start2 >= start && start2 <= end) || (end2 >= start || end2 <= end)){
+						//make sure they are non-overlapping; i.e. select the temporaries with
+						//the largest size of contiguous instructions and remove the temporaries
+						//that interfere with it (may or may not be an operand)
+						blocks[j] = null;
+						tmps[j] = null;
+					}
+				}
+			}
+			
+			//note: we could merge this loop with the previous and maintain functionality,
+			//but separating the two loops maintains readability
+			for(int i = 0; i < blocks.length; i++){
+				final ArrayList<AbstractInsnNode> block = blocks[i];
+				if(block == null) continue;
+				final Temporary tmp = tmps[i];
+				
+				final ArrayList<Temporary> criticalTemporaries = tmp.getCriticalTemporaries();
+				count++;
+				manipulateStackBeforeAndAfterTemporary(mi, tmp, criticalTemporaries, block);
+				manipulateStackSwappingTemporaryOperands(mi, tmp, criticalTemporaries, block);
+				//NOTE: if you will do both: you must do the insertion one before the swapping one
+				//otherwise, it corrupts the stack
+			}
+			
+		}
+		
+		System.out.println("Manipulated the stack in " + count + " locations.");
+		
+	}
+	
+	@Deprecated
+	private void obfuscate_bugged(){
 		int totalCount = 0;
 		for(MethodInformation mi : dm.methodInformations.values()){
 			String name = dm.methodNodeToOwnerMap.get(mi.mn).name + "." + mi.mn.name + mi.mn.desc;
@@ -55,12 +111,21 @@ public class StackManipulatorBeta implements Opcodes {
 					goodTargets.put(block, tmp);
 				}
 			}
-
+			/*
+			 * !!! Somehow, treeList.entrySet().toArray() mixes up keys and values
+			 * Wasted so much time debugging something that was a flaw in the standard library -_-
+			 */
 			@SuppressWarnings("unchecked")
 			Entry<ArrayList<AbstractInsnNode>, Temporary>[] entries = (Entry<ArrayList<AbstractInsnNode>, Temporary>[]) goodTargets.entrySet().toArray(new Entry[goodTargets.size()]);
-			Set<Entry<ArrayList<AbstractInsnNode>, Temporary>> toRemove = new HashSet<>();
 
 			for(int i = 0; i < entries.length; i++){
+				ArrayList<AbstractInsnNode> list = entries[i].getValue().getContiguousBlock();
+				if(!list.equals(entries[i].getKey())){
+					System.err.println("This is the source of error");
+				}
+			}
+			
+			for(int i = 0; i < goodTargets.size(); i++){
 				final ArrayList<AbstractInsnNode> list = entries[i].getKey();
 				final int start = list.get(0).getIndex();
 				final int end = list.get(list.size() - 1).getIndex();
@@ -69,13 +134,12 @@ public class StackManipulatorBeta implements Opcodes {
 					final int start2 = list2.get(0).getIndex();
 					final int end2 = list2.get(list2.size() - 1).getIndex();
 					if((start2 >= start && start2 <= end) || (end2 >= start || end2 <= end)){
-						toRemove.add(entries[j]);
+						//make sure they are non-overlapping; i.e. select the temporaries with
+						//the largest size of contiguous instructions and remove the temporaries
+						//that interfere with it (may or may not be an operand)
+						goodTargets.remove(entries[j].getKey());
 					}
 				}
-			}
-
-			for(Entry<ArrayList<AbstractInsnNode>, Temporary> removing : toRemove){
-				goodTargets.remove(removing.getKey());
 			}
 
 			for(Entry<ArrayList<AbstractInsnNode>, Temporary> target : goodTargets.entrySet()){
@@ -131,23 +195,18 @@ public class StackManipulatorBeta implements Opcodes {
 
 	private void manipulateStackBeforeAndAfterTemporary(MethodInformation mi, Temporary t, ArrayList<Temporary> criticalTemporaries, ArrayList<AbstractInsnNode> block){
 		//guaranteed that: block contains all critical temporaries, contains t, and is contiguous
-				//unknown bug: block.last != t.declaration; fix: use block.last instead of t.declaration
-					//when inserting instructions to restore stack
+		//unknown bug: block.last != t.declaration; fix: use block.last instead of t.declaration
+		//when inserting instructions to restore stack
 		// that criticalTemporaries is sorted in the order they are pushed onto the stack
 		// that block is sorted in the order the instructions are executed
 		
 		final Tuple<InsnList, Tuple<Integer, Integer>> toInsertBefore = generateInsertion();
 		final int sizeOfResult = t.getType().getSize();
-		
+
 		final InsnList toAddAfter = new InsnList();
 		final int strategy = toInsertBefore.val2.val1 * 10 + sizeOfResult;
-		//sizeOfReturn * 10 + sizeOfResult; sizeOfReturn is first digit; result is second
-		
+		//sizeOfReturn * 10 + sizeOfResult; sizeOfReturn is first digit; result is second digit
 
-		System.err.println("Stack at end of t: " + mi.statePerInsn.get(t.getDeclaration()).val1);
-		System.err.println("\n");
-		
-		
 		switch(strategy){
 		case 0://00
 		case 1://01
@@ -172,43 +231,41 @@ public class StackManipulatorBeta implements Opcodes {
 			add(toAddAfter, DUP2_X2, POP2, POP2);
 			break;
 		}
-		
+
 		final JavaStack stackBefore = mi.statePerInsn.get(block.get(0)).val1;
 		if(toInsertBefore.val2.val2 > mi.mn.maxStack - stackBefore.size()){
 			mi.mn.maxStack += toInsertBefore.val2.val2 - (mi.mn.maxStack - stackBefore.size());
 		}
-		
+
 		final JavaStack stackAfter = mi.statePerInsn.get(block.get(block.size() - 1)).val1;
 		if(toInsertBefore.val2.val2 > mi.mn.maxStack - stackAfter.size()){
 			mi.mn.maxStack += toInsertBefore.val2.val2 - (mi.mn.maxStack - stackBefore.size());
 		}
-		
+
 		mi.mn.instructions.insertBefore(block.get(0), toInsertBefore.val1);
 		mi.mn.instructions.insert(block.get(block.size() - 1), toAddAfter);
-		
-		System.err.println("Max stack: " + mi.mn.maxStack);
 	}
-	
+
 	private int sizeBetweenInclusive(ArrayList<Temporary> critTemporaries, int f, int s){
 		int first = f < s ? f : s;
 		int second = f < s ? s : f;
-		
+
 		int size = 0;
-		
+
 		for(int i = first; i <= second; i++){
 			size += critTemporaries.get(i).getType().getSize();
 		}
-		
+
 		return size;
 	}
-	
+
 	private static final int[][][][][] STACK_RESTORATION_OPS = {
 			//size == 0
 			{},
-			
+
 			//size == 1
 			{},
-			
+
 			//size == 2
 			{
 				//S1 == 0
@@ -217,7 +274,7 @@ public class StackManipulatorBeta implements Opcodes {
 				{
 					//S2 == 0
 					{},
-					
+
 					//S2 == 1
 					{
 						//variation 1 on (2,1,1)
@@ -227,7 +284,7 @@ public class StackManipulatorBeta implements Opcodes {
 					}
 				},
 			},
-			
+
 			//size == 3
 			{
 				//S1 == 0
@@ -243,7 +300,7 @@ public class StackManipulatorBeta implements Opcodes {
 						{SWAP, DUP2_X2, POP, POP},
 						{DUP_X1, POP, DUP2_X2, POP2},
 						{DUP_X1, POP, DUP2_X2, POP, POP},
-						
+
 						{DUP_X2, POP, SWAP},
 						{DUP_X2, POP, DUP_X1, POP}
 					},
@@ -262,7 +319,7 @@ public class StackManipulatorBeta implements Opcodes {
 					}
 				},
 			},
-			
+
 			//size == 4
 			{
 				//S1 == 0
@@ -298,7 +355,7 @@ public class StackManipulatorBeta implements Opcodes {
 				}
 			},
 	};
-	
+
 	private int getMaxStackIncrement(int[] ops){
 		int max = 0;
 		int cur = 0;
@@ -328,7 +385,7 @@ public class StackManipulatorBeta implements Opcodes {
 		}
 		return max;
 	}
-	
+
 	//copied from RedundantComputationRemover
 	private int getStoreOpcode(Type t){
 		if(t == Type.BOOLEAN_TYPE ||
@@ -346,7 +403,7 @@ public class StackManipulatorBeta implements Opcodes {
 		}
 		return Opcodes.ASTORE;
 	}
-	
+
 	private int getLoadOpcode(Type t){
 		if(t == Type.BOOLEAN_TYPE ||
 				t == Type.INT_TYPE ||
@@ -364,14 +421,6 @@ public class StackManipulatorBeta implements Opcodes {
 		return Opcodes.ALOAD;
 	}
 	
-	private static String blockToText(ArrayList<AbstractInsnNode> block){
-		final StringBuilder sb = new StringBuilder();
-		for(AbstractInsnNode ain : block){
-			sb.append(ain.getIndex()).append(", ");
-		}
-		return sb.substring(0, sb.length() - 2);
-	}
-	
 	private static void swap(List<AbstractInsnNode> L1, List<AbstractInsnNode> L2){
 		final AbstractInsnNode BL1 = L1.get(0).getPrevious();
 		final AbstractInsnNode AL1 = L1.get(L1.size() - 1).getNext();
@@ -382,10 +431,10 @@ public class StackManipulatorBeta implements Opcodes {
 			//contiguous
 			L2.get(0).setPrev(BL1);
 			BL1.setNext(L2.get(0));
-			
+
 			L1.get(L1.size() - 1).setNext(AL2);
 			AL2.setPrev(L1.get(L1.size() - 1));
-			
+
 			L2.get(L2.size() - 1).setNext(L1.get(0));
 			L1.get(0).setPrev(L2.get(L2.size() - 1));
 
@@ -401,12 +450,10 @@ public class StackManipulatorBeta implements Opcodes {
 			AL1.setPrev(L2.get(L2.size() - 1));
 		}
 	}
-	
-	private void manipulateStackSwappingTemporaryOperands(MethodInformation mi, Temporary t, ArrayList<Temporary> criticalTemporaries, ArrayList<AbstractInsnNode> block){
 
-		
-		if(t instanceof ConstructorTemporary) return;
-		
+	private void manipulateStackSwappingTemporaryOperands(MethodInformation mi, Temporary t, ArrayList<Temporary> criticalTemporaries, ArrayList<AbstractInsnNode> block){
+		//if(t instanceof ConstructorTemporary) return;
+
 		int numUnique = 0;
 		outer: for(int i = 0; i < criticalTemporaries.size(); i++){
 			for(int j = i + 1; j < criticalTemporaries.size(); j++){
@@ -418,7 +465,7 @@ public class StackManipulatorBeta implements Opcodes {
 			numUnique++;
 		}
 		if(numUnique <= 1) return;
-		
+
 		//at least 2 unique operands
 		int firstIndex = r.nextInt(criticalTemporaries.size()); //index of first operand to swap
 		int secondIndex; //index of second operand to swap
@@ -429,15 +476,12 @@ public class StackManipulatorBeta implements Opcodes {
 			secondIndex = r.nextInt(criticalTemporaries.size()); //more efficient way? oh well...
 			if(secondIndex == firstIndex) continue;
 			second = criticalTemporaries.get(secondIndex);
-			if(first.getType().getSize() == 2 && second.getType().getSize() == 2){
-				System.err.println("debug");
-			}
 			sizeBetweenInclusive = sizeBetweenInclusive(criticalTemporaries, firstIndex, secondIndex);
 			if(sizeBetweenInclusive > 4) continue;
 			break;
 		}
 		//size MUST be less than or equal to 4
-		
+
 		if(firstIndex > secondIndex){
 			int tmp = firstIndex;
 			Temporary TMP = first;
@@ -446,15 +490,14 @@ public class StackManipulatorBeta implements Opcodes {
 			secondIndex = tmp;
 			second = TMP;
 		}
-		
+
 
 		final ArrayList<AbstractInsnNode> F = first.getContiguousBlock();
 		final ArrayList<AbstractInsnNode> S = second.getContiguousBlock();
-		
+
 		swap(F, S);
-		
+
 		//insert instructions to swap at runtime before declaration
-		System.out.println("(" + sizeBetweenInclusive + ", " + first.getType().getSize() + ", " + second.getType().getSize() + ")");
 		int[][] toRandomize = STACK_RESTORATION_OPS[sizeBetweenInclusive][first.getType().getSize()][second.getType().getSize()];
 		if(toRandomize == null){
 			if(criticalTemporaries.get(firstIndex + 1).getType().getSize() == 2){
@@ -478,7 +521,7 @@ public class StackManipulatorBeta implements Opcodes {
 					toAdd.add(new InsnNode(POP));
 				}
 				mi.mn.instructions.insertBefore(F.get(0), toAdd);		
-				
+
 				toAdd = new InsnList();
 				if(r.nextBoolean()){
 					toAdd.add(new InsnNode(SWAP));
@@ -505,116 +548,6 @@ public class StackManipulatorBeta implements Opcodes {
 			add(toAdd, selectedRandom);
 			mi.mn.maxStack += getMaxStackIncrement(selectedRandom);
 			mi.mn.instructions.insert(F.get(F.size() - 1), toAdd); //add after F; look at remember above
-		}
-		System.err.println("BOOGALOGA");
-	}
-
-	public void obfuscate_poop(){
-		//TODO
-		for(MethodInformation mi : dm.methodInformations.values()){
-			TreeMap<ArrayList<AbstractInsnNode>, Temporary> tmps = new TreeMap<>((list1, list2) -> list1.size() - list2.size());
-			for(Temporary t : mi.temporaries){
-				ArrayList<AbstractInsnNode> blocks = t.getContiguousBlock();
-				if(blocks != null){
-					tmps.put(blocks, t);
-				}
-			}
-			//Map<Temporary, ArrayList<AbstractInsnNode>> tmps = mi.temporaries.stream().collect(Collectors.toMap(item -> item, Temporary::getContiguousBlock));
-			for(Entry<ArrayList<AbstractInsnNode>, Temporary> entry : tmps.entrySet()){
-				if(entry.getValue() instanceof DataManager.MethodInvocationTemporary){
-					if((Object)entry.getValue() != mi){
-						continue;
-					}
-					MethodInvocationTemporary mit = (MethodInvocationTemporary) entry.getValue();
-					Temporary[] args = mit.getArgs();
-					if(args.length == 2){
-						for(AbstractInsnNode ain : entry.getKey()){
-							System.err.println(Textifier.OPCODES[ain.getOpcode()]);
-						}
-
-						Temporary first = args[0];
-						Temporary second = args[1];
-
-						int swapOpcode = Opcodes.SWAP; //TODO handle doubles and longs
-
-						ArrayList<AbstractInsnNode> F = first.getContiguousBlock();
-						ArrayList<AbstractInsnNode> S = second.getContiguousBlock();
-
-						if(F.size() > 1 && S.size() > 1){
-							// -->F[]<-->S[]<-->CALL<--
-							// tmp = F[first].prev
-							// F[first].prev = S[last]
-							// S[last].next = F[first]
-							// F[last].next = CALL[first]
-							// S[first].prev = tmp
-							// CALL[first].prev = F[last]
-							// tmp.next = S[first]
-							AbstractInsnNode tmp = F.get(0).getPrevious();
-							F.get(0).setPrev(S.get(S.size() - 1));
-							S.get(S.size() - 1).setNext(F.get(0));
-							F.get(F.size() - 1).setNext(entry.getValue().getDeclaration());
-							S.get(0).setPrev(tmp);
-							entry.getValue().getDeclaration().setPrev(F.get(F.size() - 1));
-							tmp.setNext(S.get(0));
-						}else if((F.size() == 0 || F.size() == 1) && S.size() > 1){
-							AbstractInsnNode f = S.get(0).getPrevious();
-							// -->f<-->S[]<-->CALL<--
-							// //f = S[first].prev
-							// S[first].prev = f.prev
-							// S[last].next = f
-							// f.prev.next = S[first]
-							// f.prev = S[last]
-							// f.next = CALL
-							// CALL.prev = f
-							S.get(0).setPrev(f.getPrevious());
-							S.get(S.size() - 1).setNext(f);
-							if(f.getPrevious() != null)
-								f.getPrevious().setNext(S.get(0));
-							f.setPrev(S.get(0));
-							f.setNext(entry.getValue().getDeclaration());
-							entry.getValue().getDeclaration().setPrev(f);
-						}else if((S.size() == 0 || S.size() == 1) && F.size() > 1){
-							AbstractInsnNode s = F.get(F.size() - 1).getNext();
-							// -->F[]<-->S<-->CALL<--
-							// //s = F[last].next
-							// s.prev = F[first].prev
-							// s.next = F[first]
-							// F[first].prev.next = s
-							// F[first].prev = s
-							// F[last].next = CALL
-							// CALL.prev = F[last]
-							s.setPrev(F.get(0).getPrevious());
-							s.setNext(F.get(0));
-							F.get(0).getPrevious().setNext(s);
-							F.get(0).setPrev(s);
-							F.get(F.size() - 1).setNext(entry.getValue().getDeclaration());
-							entry.getValue().getDeclaration().setPrev(F.get(F.size() - 1));
-						}else if((F.size() == 0 || F.size() == 1) && (S.size() == 0 || S.size() == 1)){
-							// -->F<-->S<-->CALL<--
-							// s = CALL.prev
-							// f = s.prev
-							// s.prev = f.prev
-							// s.next = f
-							// f.prev = s
-							// f.next = CALL
-							AbstractInsnNode s = entry.getValue().getDeclaration().getPrevious();
-							AbstractInsnNode f = s.getPrevious();
-							s.setPrev(f.getPrevious());
-							s.setNext(f);
-							f.setPrev(s);
-							f.setNext(entry.getValue().getDeclaration());
-						}
-						//swapped
-
-						mi.mn.instructions.insertBefore(entry.getValue().getDeclaration(), new InsnNode(swapOpcode));
-
-
-						System.err.println(dm.methodNodeToOwnerMap.get(mi.mn).name + "." + mi.mn.name + mi.mn.desc);
-						System.err.println(entry.getValue());
-
-					}
-				}
-			}
 		}
 	}
 
